7ebe84c2f0e42cc6424698e1bc17bba48e3d90d7
Flavius Pantucu <flaviuspantucu@icloud.com>
2024-01-23 15:14:30 +0200

    batch verification

--- src/app/masonry-grid/masonry-grid.component.ts
+++ src/app/masonry-grid/masonry-grid.component.ts
@@ -17,6 +17,8 @@ export class MasonryGridComponent {
   current_batch: any = [];
   next_batch: any = [];
   
+  batchTimeout: boolean = false;
+
   constructor(private masonryGridService: MasonryGridService){ 
   }
 
@@ -69,17 +71,25 @@ export class MasonryGridComponent {
   public onScroll(){
     let pos = document.documentElement.scrollTop + window.innerHeight;
     let max = document.documentElement.scrollHeight;
-    if(pos / max > 0.75){
+    
+    if(pos / max > 0.75 && this.batchTimeout == false){
+      this.batchTimeout = true;
       this.masonryGridService.getNextBatch().subscribe({
         next: (data: any) =>
         {
-          this.previous_batch = this.current_batch;
-          this.current_batch = data;
-          this.loadNextBatch();
+          if(this.previous_batch.length == 0){
+            this.previous_batch = this.current_batch;
+            this.current_batch = data;
+            this.loadNextBatch();
+          }
+          else{
+            console.log("TBD");
+          }
         },
         error: (error: any) =>
           console.log(error)
       });
+      setTimeout(() => this.batchTimeout = false,1500);
     }
   }
 }
